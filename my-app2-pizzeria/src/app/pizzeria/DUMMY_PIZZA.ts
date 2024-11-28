fddc1459ca6967c6269a60461891075c6c6c005e
S73F <97076632+S73F@users.noreply.github.com>
2024-11-28 21:12:18 +0100

    Added my-app2-pizzeria

---
+++ my-app2-pizzeria/src/app/pizzeria/DUMMY_PIZZA.ts
@@ -0,0 +1,75 @@
+import { type Pizza } from './pizza/pizza.model';
+
+export const DUMMY_PIZZA = [
+  {
+    id: 1,
+    nome: 'Margherita',
+    ingredienti: 'Pomodoro, mozzarella',
+    prezzo: 5.0,
+    disponibilita: true,
+  },
+  {
+    id: 2,
+    nome: 'Diavola',
+    ingredienti: 'Pomodoro, mozzarella, salame piccante',
+    prezzo: 6.5,
+    disponibilita: true,
+  },
+  {
+    id: 3,
+    nome: 'Quattro Formaggi',
+    ingredienti: 'Mozzarella, gorgonzola, parmigiano, provola',
+    prezzo: 7.0,
+    disponibilita: false,
+  },
+  {
+    id: 4,
+    nome: 'Capricciosa',
+    ingredienti:
+      'Pomodoro, mozzarella, prosciutto cotto, funghi, carciofi, olive',
+    prezzo: 7.5,
+    disponibilita: true,
+  },
+  {
+    id: 5,
+    nome: 'Vegetariana',
+    ingredienti: 'Pomodoro, mozzarella, verdure grigliate',
+    prezzo: 6.0,
+    disponibilita: true,
+  },
+  {
+    id: 6,
+    nome: 'Tonno e Cipolla',
+    ingredienti: 'Pomodoro, mozzarella, tonno, cipolla',
+    prezzo: 7.0,
+    disponibilita: false,
+  },
+  {
+    id: 7,
+    nome: 'Prosciutto e Funghi',
+    ingredienti: 'Pomodoro, mozzarella, prosciutto cotto, funghi',
+    prezzo: 6.5,
+    disponibilita: true,
+  },
+  {
+    id: 8,
+    nome: 'Hawaiana',
+    ingredienti: 'Pomodoro, mozzarella, prosciutto cotto, ananas',
+    prezzo: 7.0,
+    disponibilita: true,
+  },
+  {
+    id: 9,
+    nome: 'Boscaiola',
+    ingredienti: 'Pomodoro, mozzarella, salsiccia, funghi',
+    prezzo: 8.0,
+    disponibilita: true,
+  },
+  {
+    id: 10,
+    nome: 'Calzone',
+    ingredienti: 'Pomodoro, mozzarella, prosciutto cotto, ricotta',
+    prezzo: 7.5,
+    disponibilita: false,
+  },
+];
---
+++ my-app2-pizzeria/src/app/pizzeria/pizza/pizza.model.ts
@@ -0,0 +1,7 @@
+export interface Pizza {
+  id: number;
+  nome: string;
+  ingredienti: string;
+  prezzo: number;
+  disponibilita: boolean;
+}
