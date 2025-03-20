36040608f597161a66dad85f47ec681bccc32b63
Stefano Cherio <scherioyt@gmail.com>
2025-03-20 09:52:57 +0100

    replaced storico ordini table with mui table

--- cmspordini-inertia-themed/resources/js/Components/Tables/StoricoOrdiniTable.jsx
+++ cmspordini-inertia-themed/resources/js/Components/Tables/StoricoOrdiniTable.jsx
@@ -1,34 +1,165 @@
-import React from "react";
-import "../../../css/table.css";
-import DataTable from "react-data-table-component";
-import useStoricoOrdiniTable from "../../Hooks/Components/Tables/useStoricoOrdiniTable";
-import StoricoOrdiniTableExpanded from "./StoricoOrdiniTableExpanded";
-import { SearchBox } from "./SearchBox";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import Box from "@mui/material/Box";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilePdf } from "@fortawesome/free-regular-svg-icons";
+import { Button } from "@mui/material";
 
-export default function StoricoOrdiniTable({ ordini }) {
-    const { records, columns, handleFilter } = useStoricoOrdiniTable({
-        ordini,
-    });
+function Row({ ordine }) {
+    const [open, setOpen] = useState(false);
 
     return (
         <>
-            <SearchBox handleFilter={handleFilter} />
-            <DataTable
-                className="custom-table"
-                columns={columns}
-                data={records}
-                expandableRows
-                expandOnRowClicked
-                expandableRowsComponent={StoricoOrdiniTableExpanded}
-                pagination
-                paginationComponentOptions={{
-                    rowsPerPageText: "Righe per pagina",
-                    rangeSeparatorText: "di",
-                    selectAllRowsItem: true,
-                    selectAllRowsItemText: "Tutte",
-                }}
-                fixedHeader
-            ></DataTable>
+            <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
+                <TableCell>
+                    <IconButton
+                        aria-label="expand row"
+                        size="small"
+                        onClick={() => setOpen(!open)}
+                    >
+                        {open ? (
+                            <KeyboardArrowUpIcon />
+                        ) : (
+                            <KeyboardArrowDownIcon />
+                        )}
+                    </IconButton>
+                </TableCell>
+                <TableCell component="th" scope="row" align="center">
+                    {ordine.data}
+                </TableCell>
+                <TableCell align="center">{ordine.medicoOrdinante}</TableCell>
+                <TableCell align="center">
+                    {ordine.data_inizioLavorazione}
+                </TableCell>
+                <TableCell align="center">
+                    {ordine.stato === 0
+                        ? "Nuovo"
+                        : ordine.stato === 1
+                        ? "In lavorazione"
+                        : "Spedito"}
+                </TableCell>
+                <TableCell align="center">{ordine.data_spedizione}</TableCell>
+                <TableCell align="center">
+                    <Button
+                        component="a"
+                        href={`/cliente/ordini/pdf/${ordine.IDordine}`}
+                        target="_blank"
+                    >
+                        <FontAwesomeIcon icon={faFilePdf} size="2xl" />
+                    </Button>
+                </TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell
+                    style={{ paddingBottom: 0, paddingTop: 0 }}
+                    colSpan={6}
+                >
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <Box sx={{ margin: 1 }}>
+                            <Typography
+                                variant="h6"
+                                gutterBottom
+                                component="div"
+                            >
+                                Dettagli ordine
+                            </Typography>
+                            <Table size="small" aria-label="dettagli-ordine">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Nome paziente</TableCell>
+                                        <TableCell>Cognome paziente</TableCell>
+                                        <TableCell align="center">
+                                            Indirizzo di spedizione
+                                        </TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    <TableRow key={ordine.IDordine}>
+                                        <TableCell component="th" scope="row">
+                                            {ordine.PazienteNome}
+                                        </TableCell>
+                                        <TableCell>
+                                            {ordine.PazienteCognome}
+                                        </TableCell>
+                                        <TableCell align="center">
+                                            {ordine.IndirizzoSpedizione}
+                                        </TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </Box>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
         </>
+    );
+}
+
+export default function CollapsibleTable({ ordini }) {
+    console.log(ordini);
+
+    return (
+        <TableContainer component={Paper}>
+            <Table aria-label="collapsible table">
+                <TableHead sx={{ bgcolor: "#1976d2" }}>
+                    <TableRow>
+                        <TableCell />
+                        <TableCell
+                            sx={{ color: "#fff", fontWeight: "bold" }}
+                            align="center"
+                        >
+                            Data Ordine
+                        </TableCell>
+                        <TableCell
+                            sx={{ color: "#fff", fontWeight: "bold" }}
+                            align="center"
+                        >
+                            Richiedente
+                        </TableCell>
+                        <TableCell
+                            sx={{ color: "#fff", fontWeight: "bold" }}
+                            align="center"
+                        >
+                            Inizio lavoro
+                        </TableCell>
+                        <TableCell
+                            sx={{ color: "#fff", fontWeight: "bold" }}
+                            align="center"
+                        >
+                            Stato lavoro
+                        </TableCell>
+                        <TableCell
+                            sx={{ color: "#fff", fontWeight: "bold" }}
+                            align="center"
+                        >
+                            Spedizione
+                        </TableCell>
+                        <TableCell
+                            sx={{ color: "#fff", fontWeight: "bold" }}
+                            align="center"
+                        >
+                            PDF
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {ordini.map((ordine) => (
+                        <Row key={ordine.IDordine} ordine={ordine} />
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
 }
