d00ec82919487402628166cf7fcf03f2cf49188f
Stefano Cherio <scherioyt@gmail.com>
2025-03-21 13:58:05 +0100

    edited operatore dashboard buttons

--- cmspordini-inertia-themed/resources/js/Pages/Operatore/Dashboard.jsx
+++ cmspordini-inertia-themed/resources/js/Pages/Operatore/Dashboard.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "../../../css/operatoreDashboard.css";
-import { Link } from "@inertiajs/react";
 import Notification from "../../Components/Notification";
 import { useDashboard } from "../../Hooks/Operatore/useDashboard";
 import { Lavori } from "../../Components/Tables/Lavori";
 import OperatoreLayout from "../../Layouts/OperatoreLayout";
 import { PaperContainer } from "../../Components/PaperContainer";
-import { Typography } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
+import {
+    AssignmentTurnedIn as AssignmentTurnedInIcon,
+    Loop as LoopIcon,
+    NewReleases as NewReleasesIcon,
+    CheckCircle as CheckCircleIcon,
+} from "@mui/icons-material";
 
 export default function Dashboard({ user, tipo, lavori, numLavoriNuovi }) {
     const { handleLavori } = useDashboard();
@@ -18,6 +23,60 @@ export default function Dashboard({ user, tipo, lavori, numLavoriNuovi }) {
                     ? `Benvenuto ${user?.nome}`
                     : `Benvenuto ${user?.cognome ?? "Utente"}`}
             </Typography>
+
+            <Stack direction="row" spacing={2} justifyContent="center">
+                <Button
+                    startIcon={<NewReleasesIcon />}
+                    size="large"
+                    onClick={() => handleLavori("nuovi")}
+                    variant="contained"
+                    color="primary"
+                    sx={{
+                        fontWeight: "bold",
+                        px: 3,
+                        py: 1,
+                        width: 170,
+                        textAlign: "left",
+                        gap: 1,
+                    }}
+                >
+                    Lavori nuovi
+                </Button>
+                <Button
+                    startIcon={<LoopIcon />}
+                    size="large"
+                    onClick={() => handleLavori("inCorso")}
+                    variant="contained"
+                    color="primary"
+                    sx={{
+                        fontWeight: "bold",
+                        px: 3,
+                        py: 1,
+                        width: 170,
+                        textAlign: "left",
+                        gap: 1,
+                    }}
+                >
+                    Lavori in corso
+                </Button>
+                <Button
+                    startIcon={<CheckCircleIcon />}
+                    size="large"
+                    onClick={() => handleLavori("terminati")}
+                    variant="contained"
+                    color="primary"
+                    sx={{
+                        fontWeight: "bold",
+                        px: 3,
+                        py: 1,
+                        width: 160,
+                        textAlign: "left",
+                        gap: 1,
+                    }}
+                >
+                    Lavori terminati
+                </Button>
+            </Stack>
         </PaperContainer>
     );
 
