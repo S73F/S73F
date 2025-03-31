3fecef2b2010599d35a41c7fa1490195a4e25880
Stefano Cherio <scherioyt@gmail.com>
2025-03-31 17:29:38 +0200

    added comments to useEliminazioneCliente hook

--- cmspordini-inertia-themed/resources/js/Hooks/Components/Modals/useEliminazioneCliente.jsx
+++ cmspordini-inertia-themed/resources/js/Hooks/Components/Modals/useEliminazioneCliente.jsx
@@ -1,15 +1,29 @@
 import { router } from "@inertiajs/react";
 
+/**
+ * Hook personalizzato per gestire l'eliminazione di un cliente.
+ *
+ * @param {Object} params - Oggetto contenente i parametri necessari.
+ * @param {Object} params.cliente - Dati del cliente da eliminare.
+ * @param {Object} params.modalRef - Riferimento alla modale di conferma eliminazione.
+ * @returns {Object} Oggetto con le funzioni per eliminare il cliente e chiudere il modale.
+ */
 export const useEliminazioneCliente = ({ cliente, modalRef }) => {
+    /**
+     * Funzione per eliminare un cliente effettuando una richiesta DELETE al server.
+     * @param {Event} e - Evento del form.
+     */
     const handleDelete = (e) => {
         e.preventDefault();
+
+        // Effettua la richiesta DELETE utilizzando Inertia.js
         router.delete(
             `/operatore/gestione-clienti/eliminazione/${cliente.IDcliente}`,
             {
-                only: ["clienti", "flash"],
+                only: ["clienti", "flash"], // Aggiorna solo la lista clienti e i messaggi flash
                 preserveScroll: true,
                 preserveState: true,
-                onSuccess: () => closeModal(),
+                onSuccess: () => closeModal(), // Chiude la modale in caso di successo.
                 onError: (errors) => {
                     console.log(errors);
                 },
@@ -17,6 +31,9 @@ export const useEliminazioneCliente = ({ cliente, modalRef }) => {
         );
     };
 
+    /**
+     * Chiude la modale accedendo direttamente al suo elemento DOM tramite il riferimento "modalRef".
+     */
     const closeModal = () => {
         modalRef.current.close();
     };
