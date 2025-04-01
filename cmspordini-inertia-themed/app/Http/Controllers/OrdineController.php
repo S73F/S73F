3f776ba29a83a5a86830801e55a7e34124b99628
Stefano Cherio <scherioyt@gmail.com>
2025-04-01 16:51:15 +0200

    deleted useless comments in ordinecontroller

--- cmspordini-inertia-themed/app/Http/Controllers/OrdineController.php
+++ cmspordini-inertia-themed/app/Http/Controllers/OrdineController.php
@@ -11,7 +11,6 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
-use Inertia\Inertia;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Support\Facades\Storage;
 
@@ -20,15 +19,18 @@ class OrdineController extends Controller
 
     /**
      * Crea un nuovo ordine e gestisce il file caricato.
-     * 
+     *
+     * Valida i dati in ingresso, calcola il numero progressivo per l'anno corrente e crea l'ordine.
+     * Se viene caricato un file, lo salva nello storage pubblico, aggiorna l'ordine e avvia un job asincrono
+     * per inviare un'email di notifica a cliente e operatori.
+     *
      * @param Request $request - La richiesta HTTP contenente i dati del nuovo ordine.
-     * @return \Illuminate\Http\RedirectResponse - Ritorna una risposta di reindirizzamento.
+     * @return \Illuminate\Http\RedirectResponse - Ritorna un redirect con un messaggio di successo o errore.
      * @throws ValidationException - Se i dati non sono validi.
      */
     public function creazione(Request $request)
     {
         try {
-            // Validazione dei dati in ingresso
             $request->validate([
                 'medico_ordinante' => 'required|string|max:50',
                 'paziente_nome' => 'required|string|max:50',
@@ -51,7 +53,6 @@ class OrdineController extends Controller
             $anno = date('Y');
             $numero = Ordine::whereYear('data', $anno)->max('numero') + 1;
 
-            // Creazione dell'ordine
             $ordine = Ordine::create([
                 'IDcliente' => Auth::guard('cliente')->user()->IDcliente,
                 'numero' => $numero,
@@ -71,39 +72,32 @@ class OrdineController extends Controller
                 'note_int' => '',
             ]);
 
-            // Se la richiesta contiene un file valido, salva il file nello storage pubblico
+            // Se viene caricato un file valido, lo salva e aggiorna l'ordine
             if ($request->hasFile('userfile') && $request->file('userfile')->isValid()) {
-                // Recupera il file caricato
                 $file = $request->file('userfile');
-
-                // Recupera l'estensione del file
                 $extension = $file->getClientOriginalExtension();
-
-                // Assegna il nome al file sorgente
                 $newFileName = Auth::guard('cliente')->user()->ragione_sociale . "_" . strtoupper($request->paziente_cognome) . "_" . strtoupper($request->paziente_nome) . "_" . $ordine->IDordine . "." . $extension;
 
                 // Salva il file nella cartella storage/app/public/uploads
                 $file->storeAs('uploads', $newFileName, 'public');
 
-                // Aggiorna l'ordine con il nome del file
                 $ordine->update([
                     'fileok' => 1,
                     'nomefile' => $newFileName,
                 ]);
 
-                // Seleziona tutte le mail degli operatori presenti nel database
+                // Recupera tutte le email degli operatori
                 $mailOperatori = Operatore::pluck("emailoperatore");
 
-                // Avvia un job asincrono per inviare un'email di notifica al cliente e agli operatori,  
-                // informandoli della creazione del nuovo ordine.
+                // Dispatch asincrono per inviare un'email di notifica a cliente e operatori
                 dispatch(new CreazioneOrdineMailJob(['mailCliente' => Auth::guard('cliente')->user()->emailcliente, 'ragioneSociale' => Auth::guard('cliente')->user()->ragione_sociale, 'numero' => $numero, 'anno' => $anno, 'mailOperatori' => $mailOperatori]));
 
                 return redirect('/cliente/dashboard')->with(['success' => 'Ordine creato e file caricato con successo!']);
             } else {
                 return redirect('/cliente/dashboard')->with(['success' => 'Ordine creato con successo!']);
             }
+
         } catch (ValidationException $e) {
-            // Log tramite toast degli errori
             $errors = $e->validator->errors();
             return redirect()->back()->with(["error" => "Errore durante la creazione dell'ordine", "validation_errors" => $errors])->withInput();
         }
@@ -116,7 +110,6 @@ class OrdineController extends Controller
      */
     public function getNumeroLavori()
     {
-        // Recupera il numero di lavori nuovi dal database
         $numeroLavoriNuovi = Ordine::where('stato', 0)->count();
 
         return $numeroLavoriNuovi;
@@ -125,77 +118,69 @@ class OrdineController extends Controller
     /**
      * Genera un PDF dell'ordine.
      * 
+     * Recupera l'ordine con i campi specificati, aggiungendo ulteriori dati se l'utente loggato è un operatore.
+     * Quindi, genera il PDF utilizzando la vista PDF.ordinePDF e lo restituisce come stream.
+     *
      * @param int $id - ID dell'ordine.
      * @return \Illuminate\Http\Response - Risposta con il PDF generato.
      */
     public function generaPDF($id)
     {
-        // Definizione dei campi da restituire alla vista del PDF
+        // Campi da restituire alla vista del PDF
         $fields = ['IDordine', 'IDcliente', 'numero', 'data', 'medicoOrdinante', 'PazienteNome', 'PazienteCognome', 'IndirizzoSpedizione', 'lavorazione', 'colore', 'piattaforma', 'data_cons', 'ora_cons', 'note'];
 
-        // Se l'utente loggato è un operatore, aggiunge all'array $fields i campi seguenti
+        // Se l'utente loggato è un operatore, aggiunge i campi seguenti
         if (Auth::guard('operatore')->check()) {
             $fields = array_merge($fields, ['utente_modifica', 'note_int', 'note_ulti_mod']);
         }
 
-        // Recupera i dati dell'ordine selezionato
         $ordine = Ordine::select($fields)->with('cliente:IDcliente,ragione_sociale,indirizzo,citta,provincia')->find($id);
 
-        // Generazione del PDF con i dati presenti nei campi selezionati
         $pdf = Pdf::loadView("PDF.ordinePDF", compact("ordine"));
 
-        // Viene aperto il PDF all'interno di una nuova finestra del browser
         return $pdf->stream("ordine_{$ordine->IDordine}.pdf");
     }
 
     /**
      * Aggiorna lo stato di un ordine, avanzando allo stato successivo o annullando l'ordine.
      * 
+     * Se l'opzione è "forward", il metodo aggiorna lo stato:
+     * - Se lo stato corrente è 0, lo imposta a 1, registra la data di inizio lavorazione e assegna l'ordine all'operatore.\n
+     * - Se lo stato corrente è 1, lo imposta a 2, registra la data di spedizione e invia una notifica via email.\n
+     * Se l'opzione non è "forward" (ad esempio "back"), tutti i dati dell'ordine (compreso lo stato) vengono ripristinati.
+     *
      * @param Request $request - La richiesta HTTP contenente i dati dell'operatore.
      * @param int $IDordine - ID dell'ordine da aggiornare.
-     * @param string $option - Opzione per avanzare o annullare l'ordine (default è "forward").
-     * @return \Illuminate\Http\RedirectResponse - Ritorna una risposta di reindirizzamento.
-     * @throws Exception - Se si verifica un errore durante l'aggiornamento.
+     * @param string $option - Opzione per avanzare o annullare l'ordine (default "forward").
+     * @return \Illuminate\Http\RedirectResponse - Redirect con un messaggio di conferma o errore.
+     * @throws Exception - Se lo stato dell'ordine non è valido.
      */
     public function aggiornaStato(Request $request, $IDordine, $option = "forward")
     {
-        // Recupera i dati dell'ordine selezionato
         $ordine = Ordine::select('IDordine', 'IDoperatore', 'IDcliente', 'numero', 'data', 'stato')->with(['operatore:IDoperatore,nome,cognome', 'cliente:IDcliente,emailcliente'])->find($IDordine);
 
-        // Se l'ordine non esiste nel database, effettua un redirect con l'errore
         if (!$ordine) {
             return redirect()->back()->withErrors('Ordine non trovato');
         }
 
         try {
-            // Se a $option viene passato come argomento "forward", effettua le successive verifiche
             if ($option == "forward") {
 
                 switch ($ordine->stato) {
-                    // Se l'ordine è nello stato iniziale (0), aggiorna lo stato impostandolo a "In lavorazione" (1),  
-                    // registra la data di inizio lavorazione e assegna l'ordine all'operatore corrente.  
-                    // Infine, effettua un redirect alla pagina corrente con un messaggio di conferma.
+
+                    // Stato iniziale: passa a "In lavorazione" (1), registra la data di inizio e assegna l'ordine all'operatore corrente
                     case 0:
                         $ordine->update(['stato' => 1, 'data_inizioLavorazione' => now(), 'IDoperatore' => $request->user()->IDoperatore]);
-
                         return redirect('/operatore/dashboard?tipo=nuovi')->with('success', 'Hai preso in carico il lavoro.');
 
-
-                    // Se l'ordine è nello stato "In lavorazione" (1), aggiorna lo stato impostandolo a "Spedito" (2)  
-                    // e registra la data di spedizione.  
-                    // Invia inoltre un'email di notifica al cliente con i dettagli dell'ordine e dell'operatore responsabile.  
-                    // Infine, effettua un redirect con un messaggio di conferma.
+                    // Stato "In lavorazione": passa a "Spedito" (2), registra la data di spedizione
                     case 1:
                         $ordine->update(['stato' => 2, 'data_spedizione' => now()]);
 
-                        // Avvia un job asincrono per inviare un'email di notifica al cliente,  
-                        // informandolo della spedizione dell'ordine.
+                        // Dispatch asincrono per inviare un'email di notifica al cliente
                         dispatch(new TermineOrdineMailJob(['mailCliente' => $ordine->cliente->emailcliente, 'numero' => $ordine->numero, 'anno' => Carbon::parse($ordine->data)->format('Y'), 'nomeOperatore' => $ordine->operatore->nome, 'cognomeOperatore' => $ordine->operatore->cognome]));
-
                         return redirect('/operatore/dashboard?tipo=inCorso')->with('success', 'Hai spedito la lavorazione.');
 
-
-                    // Se lo stato dell'ordine non corrisponde né a 0 né a 1, viene lanciata un'eccezione
                     default:
                         throw new Exception("Stato dell'ordine non valido. Contattare un amministratore");
                 }
@@ -207,7 +192,6 @@ class OrdineController extends Controller
             }
 
         } catch (Exception $e) {
-            // Gestione degli errori
             $errors = $e->getMessage();
             return redirect()->back()->with('error', 'ATTENZIONE: si è verificato un errore. Riprova più tardi.')->withErrors($errors);
         }
@@ -221,19 +205,16 @@ class OrdineController extends Controller
      */
     public function downloadFile($id)
     {
-        // Seleziona il nome del file corrispondente all'ordine selezionato
+        // Recupera il nome del file associato all'ordine dal database
         $fileName = Ordine::where('IDordine', $id)->value('nomefile');
 
-        // Se il nome del file non viene trovato nel database, effettua un redirect con l'errore
         if (!$fileName) {
             return redirect()->back()->with(['error' => 'File non trovato nel database.']);
         }
 
-        // Se il file esiste nello storage pubblico, effettua il download del file
         if (Storage::disk('public')->exists("uploads/{$fileName}")) {
             return Storage::disk('public')->download("uploads/{$fileName}");
         } else {
-            // Se il nome del file viene trovato nel database ma non nello storage, effettua un redirect con l'errore
             return redirect()->back()->with(['error' => 'Il file non esiste nel server.']);
         }
     }
@@ -246,19 +227,16 @@ class OrdineController extends Controller
      */
     public function downloadFileFinale($id)
     {
-        // Seleziona il nome del file finale corrispondente all'ordine selezionato
+        // Recupera il nome del file finale associato all'ordine dal database
         $fileName = Ordine::where('IDordine', $id)->value('file_fin_nome');
 
-        // Se il nome del file finale non viene trovato nel database, effettua un redirect con l'errore
         if (!$fileName) {
             return redirect()->back()->with(['error' => 'File non trovato nel database.']);
         }
 
-        // Se il file finale esiste nello storage pubblico, effettua il download del file finale
         if (Storage::disk('public')->exists("uploads/{$fileName}")) {
             return Storage::disk('public')->download("uploads/{$fileName}");
         } else {
-            // Se il nome del file finale viene trovato nel database ma non nello storage, effettua un redirect con l'errore
             return redirect()->back()->with(['error' => 'Il file non esiste nel server.']);
         }
     }
