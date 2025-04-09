3a37f1753498bcd10e20c5f58157ce29eb0da970
Stefano Cherio <scherioyt@gmail.com>
2025-04-09 10:44:28 +0200

    added filament project with migrations

---
+++ cmspordini-filament/database/migrations/2025_04_09_075843_create_clienti_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clienti', function (Blueprint $table) {
+            $table->id('idCliente')->primary();
+            $table->foreignId('id')->constrained('users')->onDelete('cascade'); // Foreign key to the users table
+            $table->string('ragioneSociale', 100);
+            $table->string('nome', 50)->nullable();
+            $table->string('cognome', 50)->nullable();
+            $table->string('partitaIva', 50);
+            $table->string('indirizzo', 50);
+            $table->string('citta', 50);
+            $table->integer('cap');
+            $table->string('provincia', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clienti');
+    }
+};
---
+++ cmspordini-filament/database/migrations/2025_04_09_075926_create_operatori_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('operatori', function (Blueprint $table) {
+            $table->id('idOperatore')->primary();
+            $table->foreignId('id')->constrained('users')->onDelete('cascade'); // Foreign key to the users table
+            $table->string('nome', 50);
+            $table->string('cognome', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('operatori');
+    }
+};
